2c53f57efb9b9e785b8981951f80ca98395e9e4f
Vu <builevutam4494@gmail.com>
2016-03-19 15:40:09 +0700

    Add product model test, Update product model to pass the test

--- server/api/product/product.model.js
+++ server/api/product/product.model.js
@@ -3,9 +3,21 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var ProductSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  title: {
+  	type: String,
+  	required: true,
+  	trim: true
+  },
+  price: {
+  	type: Number,
+  	required: true,
+  	min: 0
+  },
+	stock: {
+		type: Number,
+		default: 1	
+	},
+	description: String
 });
 
 export default mongoose.model('Product', ProductSchema);
